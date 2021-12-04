4c96aa3065141442e20066cbad23e707d85a4e30
Shaadi Alfred <shaadi.alfred@gmail.com>
2021-12-04 20:40:57 +0200

    Added /api/images endpoint

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import fs from "fs";
 import { getImagesPath } from "../../helpers";
+import Sharp from "sharp";
+
+
+if (process.platform === "win32") {
+    Sharp.cache(false);
+}
+
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
     const filename = req.query.filename as string;
     const width = parseInt(req.query.width as string);
     const height = parseInt(req.query.height as string);
@@ -25,13 +32,47 @@ router.get("/", (req, res) => {
         return res.status(422).send("Height or width are not valid");
     }
 
-    const imagePath = getImagesPath(filename)
+    const fullImagePath = getImagesPath(filename)
 
-    if (! fs.existsSync(imagePath)) {
+    if (! fs.existsSync(fullImagePath)) {
         return res.status(404).send("Chosen image does not exist");
     }
 
-    res.sendFile(imagePath);
+    const thumbImagePath = getImagesPath(filename, false);
+
+    if (fs.existsSync(thumbImagePath)) {
+        let sharp = Sharp(thumbImagePath);
+
+        const metadata = await sharp.metadata();
+
+        if (metadata.height !== height || metadata.width !== width) {
+            sharp = Sharp(fullImagePath).resize(width, height);
+
+            try {
+                const bufferedImage = await sharp.toBuffer();
+                fs.writeFileSync(thumbImagePath, bufferedImage);
+            } catch (error) {
+                console.error(error);
+            }
+
+            return res.sendFile(thumbImagePath);
+        }
+
+        return res.sendFile(thumbImagePath);
+
+    }
+
+    let sharp = Sharp(fullImagePath);
+
+    sharp = sharp.resize(width, height);
+
+    try {
+        await sharp.toFile(thumbImagePath);
+    } catch (error) {
+        console.error(error);
+    }
+
+    res.sendFile(thumbImagePath);
 });
 
 export default router;
