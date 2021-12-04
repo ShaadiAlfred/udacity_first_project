2c4c90999a8a589fb82e4d0db8ee4ce52756a1c0
Shaadi Alfred <shaadi.alfred@gmail.com>
2021-12-04 17:08:07 +0200

    Add sharp as a dependency

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,9 +1,37 @@
 import express from "express";
+import fs from "fs";
+import { getImagesPath } from "../../helpers";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("Images");
+    const filename = req.query.filename as string;
+    const width = parseInt(req.query.width as string);
+    const height = parseInt(req.query.height as string);
+
+    if (
+        filename === undefined ||
+        width === undefined ||
+        height === undefined
+    ) {
+        return res.status(422).send("Missing parameters");
+    }
+
+    if (filename.length === 0) {
+        return res.status(422).send("You have to enter a filename");
+    }
+
+    if (isNaN(width) || isNaN(height)) {
+        return res.status(422).send("Height or width are not valid");
+    }
+
+    const imagePath = getImagesPath(filename)
+
+    if (! fs.existsSync(imagePath)) {
+        return res.status(404).send("Chosen image does not exist");
+    }
+
+    res.sendFile(imagePath);
 });
 
 export default router;
